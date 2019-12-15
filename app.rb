c7cf54e35006e44405eea1cf0e8cfeb39a9c2d67
dtrift <89174579997@mail.ru>
2019-12-15 09:29:22 +0500

    Time Formatter

--- app.rb
+++ app.rb
@@ -1,24 +1,32 @@
+require_relative 'time_formatter'
+
 class App
+
   def call(env)
-    perform_request
-    [status, headers, body]
+    request = Rack::Request.new(env)
+
+    case request.path_info
+    when '/time'
+      response = TimeFormat.new(request.params)
+
+      if response.unknown_formats.empty?
+        http_response(200, Time.now.strftime(response.time_format))
+      else
+        http_response(400, "Unknown format #{response.unknown_formats}")
+      end
+    else
+      http_response(404, 'Not Found')
+    end
   end
 
   private
 
-  def perform_request
-    sleep rand(1..4)
+  def http_response(status, body)
+    [
+      status,
+      { 'Content-Type' => 'text/plain' },
+      [body]
+    ]
   end
 
-  def status
-    200
-  end
-
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
-  def body
-    ["Welcome aboard!\n"]
-  end
 end
---
+++ time_formatter.rb
@@ -0,0 +1,32 @@
+class TimeFormatter
+
+  attr_reader :time_format, :unknown_formats
+
+  FORMATS = {
+    'year'   => '%Y',
+    'month'  => '%m',
+    'day'    => '%d',
+    'hour'   => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }.freeze
+
+  def initialize(params)
+    @time_format = ' '
+    @unknown_formats = []
+    set_time_output(params['format'].split(','))
+  end
+
+  private
+
+  def set_time_output(params)
+    params.each do |format|
+      if FORMATS[format]
+        @time_format += FORMATS[format]
+      else
+        @unknown_formats << format
+      end
+    end
+  end
+
+end
